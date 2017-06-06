5a9619bf6eedf27567332f2b6d9d84cf61905912
caiogranero <caio.granero@icolabora.com.br>
2017-06-06 19:20:16 -0300

    This is the second project in this course
    This new project its a simple project using react with redux.
    Simulate a bookstore, but veeeeeery simple YET!

---
+++ book-store/src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function(){
+	return [
+		{'title': 'Javascript: The Good Parts', pages: 101},
+		{'title': 'Harry Potter', pages: 38},
+		{'title': 'The Dark Tower', pages: 1}
+	];
+}
